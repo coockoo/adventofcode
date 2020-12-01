3bf8cb983304a15c6b8171981f6e59eaa09d305e
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-01 14:59:54 +0200

    Do work in one run

--- 01/index.js
+++ 01/index.js
@@ -7,14 +7,13 @@ async function main() {
   const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8')
   const arr = content.split(/\n/g).filter(i => i);
 
-  const byNumber = {}
-  arr.forEach(item => {
-    byNumber[item] = true;
-  });
+  const byNumber = {};
 
   for(let i = 0; i < arr.length; ++i) {
     const current = +arr[i]
-    if (byNumber[SUM - current]) {
+    byNumber[current] = 1;
+
+    if (byNumber[SUM - current] > 0) {
       console.log(i, current, SUM - current, current * (SUM - current));
       return;
     }
