83339f5da340fa3e16de960128a72ad71b475301
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-02 08:52:51 +0200

    Add calculation for triples

--- 01/index.js
+++ 01/index.js
@@ -5,21 +5,31 @@ const SUM = 2020;
 
 async function main() {
   const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8')
-  const arr = content.split(/\n/g).filter(i => i);
+  const arr = content.split(/\n/g).filter(i => i).map(i => +i);
 
   const byNumber = {};
 
   for(let i = 0; i < arr.length; ++i) {
-    const current = +arr[i]
+    const current = arr[i]
     byNumber[current] = 1;
+    const possible = SUM - current;
 
-    if (byNumber[SUM - current] > 0) {
-      console.log(i, current, SUM - current, current * (SUM - current));
-      return;
+    if (byNumber[possible] > 0) {
+      console.log('A:', current, 'B:', possible, 'Product:', current * possible);
+      break;
     }
   }
 
-  console.log('NO SUCH NUMBER')
+  for(let i = 0; i < arr.length; ++i) {
+    for(let j = i + 1; j < arr.length; ++j) {
+      const current = arr[i] + arr[j];
+      const possible = SUM - current;
+      if (byNumber[possible] > 0) {
+        console.log('A:', arr[i], 'B:', arr[j], 'C:', possible, 'Product:', arr[i] * arr[j] * possible);
+        break;
+      }
+    }
+  }
 }
 
 main().catch(console.error)
