ece4dd79e83544d8b6c5d8eedf2eb400b2f67867
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-02 09:19:00 +0200

    Add solution to the second part of the second day

--- 02/index.js
+++ 02/index.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 async function main() {
   const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8')
-  const arr = content.split(/\n/g).filter(i => i).map(i => {
-    const [_, min, max, letter, password] = i.match(/(\d+)\-(\d+) (\S): (.+)/)
-    return { min, max, letter, password };
+  const arr = content.split(/\n/g).filter(i => i).map(raw => {
+    const [_, min, max, letter, password] = raw.match(/(\d+)\-(\d+) (\S): (.+)/)
+    return { min, max, letter, password, raw };
   });
 
   let validPasswords = 0;
@@ -22,7 +22,17 @@ async function main() {
     }
   }
 
-  console.log('There are', validPasswords, 'valid passwords.');
+  console.log('There are', validPasswords, 'valid passwords for first policy.');
+
+  let validPasswords2 = 0;
+  for(let i = 0; i < arr.length; ++i) {
+    const { min, max, letter, password } = arr[i];
+    if (password[min - 1] === letter ^ password[max -1] === letter) {
+      validPasswords2 += 1;
+    }
+  }
+
+  console.log('There are', validPasswords2, 'valid passwords for second policy.');
 }
 
 main().catch(console.error)
