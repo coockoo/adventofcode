25e8803a8c2f1cced332f9bd5401f3311921e422
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-03 07:35:20 +0200

    Add pretty solution for 03

--- 03/index.js
+++ 03/index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
+const TREE_SYMBOL = '#';
+const shiftsY = [1,1,1,1,2];
+const shiftsX = [1,3,5,7,1];
+
 async function main() {
   const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8')
   const arr = content.split(/\n/g).filter(i => i).map(raw => {
@@ -10,62 +14,38 @@ async function main() {
     };
   });
 
-  let currentX = 0
-  let treesCount = 0;
-  for (let i = 0; i< arr.length; ++i) {
-    if (arr[i].trees[currentX % arr[i].trees.length] === '#') {
-      treesCount += 1;
+  const rowSize = arr[0].trees.length;
+
+  const treesCount = {}
+  const addTree = (x, y) => {
+    if (!treesCount[y]) {
+      treesCount[y] = {}
     }
-    currentX += 3;
+    if (!treesCount[y][x]) {
+      treesCount[y][x] = 0
+    }
+    treesCount[y][x] += 1;
+  }
+  for (let y = 0; y < arr.length; y += 1) {
+    for (let i = 0; i < shiftsY.length; i += 1) {
+      const shiftY = shiftsY[i]
+      const shiftX = shiftsX[i];
+      const x = (y * shiftX) % rowSize;
+      if (y * shiftY < arr.length && arr[y * shiftY].trees[x] === TREE_SYMBOL) {
+        addTree(shiftX, shiftY)
+      }
+    }
   }
 
-  console.log(treesCount);
+  let product = 1;
+  Object.keys(treesCount).map(y => {
+    Object.keys(treesCount[y]).map(x => {
+      console.log(`Trees for slope Right ${x}, Down ${y}: ${treesCount[y][x]}`)
+      product *= treesCount[y][x];
+    });
+  });
 
-  let treesProduct = treesCount;
-
-  currentX = 0
-  treesCount = 0;
-  for (let i = 0; i< arr.length; ++i) {
-    if (arr[i].trees[currentX % arr[i].trees.length] === '#') {
-      treesCount += 1;
-    }
-    currentX += 1;
-  }
-
-  treesProduct *= treesCount;
-
-  currentX = 0
-  treesCount = 0;
-  for (let i = 0; i< arr.length; ++i) {
-    if (arr[i].trees[currentX % arr[i].trees.length] === '#') {
-      treesCount += 1;
-    }
-    currentX += 5;
-  }
-  treesProduct *= treesCount;
-
-  currentX = 0
-  treesCount = 0;
-  for (let i = 0; i< arr.length; ++i) {
-    if (arr[i].trees[currentX % arr[i].trees.length] === '#') {
-      treesCount += 1;
-    }
-    currentX += 7;
-  }
-  treesProduct *= treesCount;
-
-  currentX = 0
-  treesCount = 0;
-  for (let i = 0; i< arr.length; i += 2) {
-    if (arr[i].trees[currentX % arr[i].trees.length] === '#') {
-      treesCount += 1;
-    }
-    currentX += 7;
-  }
-  treesProduct *= treesCount;
-
-
-  console.log(treesProduct);
+  console.log(`Total product of all slopes: ${product}`);
 }
 
 main().catch(console.error)
