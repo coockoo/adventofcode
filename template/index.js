9a671c3da8436a79125b0a3f7901a0d03f87a750
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-04 07:32:04 +0200

    Add challenge template

---
+++ template/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+async function main() {
+  const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8')
+  const arr = content.split(/\n/g).filter(i => i).map(raw => {
+    return {
+      raw
+    }
+  });
+
+  let res = 0;
+
+
+
+  console.log(res);
+}
+
+main().catch(console.error);
