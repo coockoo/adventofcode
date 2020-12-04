c8e52063efff2fb51dace03af25f1e07e41bd77f
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-04 07:46:49 +0200

    Prettify solution for day 4 a bit

--- 04/index.js
+++ 04/index.js
@@ -31,40 +31,77 @@ async function main() {
     const keys = Object.keys(passport);
     const len = keys.length;
 
-    const isValidHgt = () => {
-      if (!passport.hgt) {
-        return false;
+    const rules = [
+      requiredFields,
+      isValidByr,
+      isValidIyr,
+      isValidEyr,
+      isValidHgt,
+      isValidHcl,
+      isValidEcl,
+      isValidPid,
+    ];
+    const isPassportValid = rules.every(rule => {
+      const isValid = rule(passport);
+      if (!isValid) {
+        console.log(`PASSPORT INVALID ${rule.name}`, passport)
       }
-      const match = passport.hgt.match(/(\d{2,3})(cm|in)/);
-      if (!match) {
-        return false;
-      }
-      if (match[2] === 'in' && +match[1] >= 59 && +match[1] <= 76) {
-        return true
-      }
-      if (match[2] === 'cm' && +match[1] >= 150 && +match[1] <= 193) {
-        return true
-      }
-      return false
-    }
-
-    if (
-      (len === 8 || (len === 7 && !passport.cid))
-      && (passport.byr && passport.byr.match(/^\d{4}$/) && +passport.byr >= 1920 && +passport.byr <= 2002)
-      && (passport.iyr && passport.iyr.match(/^\d{4}$/) && +passport.iyr >= 2010 && +passport.iyr <= 2020)
-      && (passport.eyr && passport.eyr.match(/^\d{4}$/) && +passport.eyr >= 2020 && +passport.eyr <= 2030)
-      && isValidHgt()
-      && (passport.hcl && passport.hcl.match(/^#[0-9a-f]{6}$/))
-      && (passport.ecl && ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(passport.ecl))
-      && (passport.pid && passport.pid.match(/^\d{9}$/))
-    ) {
+      return isValid;
+    })
+    if (isPassportValid) {
       res += 1;
-      console.log('PASSPORT VALID', passport);
-    } else {
-      console.log('PASSPORT INVALID', passport);
     }
   }
-  console.log(res);
 }
+
+function requiredFields(passport) {
+  const keys = Object.keys(passport);
+  const len = keys.length;
+  return len === 8 || (len === 7 && !passport.cid);
+}
+
+function isValidByr(passport) {
+  return passport.byr && passport.byr.match(/^\d{4}$/) && +passport.byr >= 1920 && +passport.byr <= 2002;
+}
+
+function isValidIyr(passport) {
+  return passport.iyr && passport.iyr.match(/^\d{4}$/) && +passport.iyr >= 2010 && +passport.iyr <= 2020;
+}
+
+function isValidEyr(passport) {
+  return passport.eyr && passport.eyr.match(/^\d{4}$/) && +passport.eyr >= 2020 && +passport.eyr <= 2030;
+}
+
+function isValidHgt (passport) {
+  if (!passport.hgt) {
+    return false;
+  }
+  const match = passport.hgt.match(/^(\d{2,3})(cm|in)$/);
+  if (!match) {
+    return false;
+  }
+
+  const value = +match[1];
+  const metric = match[2];
+
+  return (
+    (metric === 'in' && 59 <= value && value <= 76)
+    || (metric === 'cm' && 150 <= value && value <= 193)
+  )
+}
+
+function isValidHcl(passport) {
+  return passport.hcl && passport.hcl.match(/^#[0-9a-f]{6}$/);
+}
+
+function isValidEcl(passport) {
+  const possibleEcls = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+  return passport.ecl && possibleEcls.includes(passport.ecl);
+}
+
+function isValidPid(passport) {
+  return !!passport.pid && !!passport.pid.match(/^\d{9}$/);
+}
+
 
 main().catch(console.error);
