88aff7a8d1820b56c2e4fae47c8156530da30db0
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-06 06:46:49 +0200

    Prettify solution for 05

--- 05/index.js
+++ 05/index.js
@@ -1,45 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 
+const ROW_SIZE = 8;
+
 async function main() {
   const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8')
-  const arr = content.split(/\n/g).filter(i => i).map(raw => {
-    return raw.split('');
-  });
+  const arr = content.split(/\n/g).filter(i => i);
 
   let maxSeatId = 0;
-
-  const allSeats = Array(128 * 8).fill().map((_, i) => i);
+  let minSeatId = 9001; // over 9000
+  const seats = {};
 
   for (let i = 0; i < arr.length; ++i) {
     const current = arr[i];
 
-    let rows = Array(128).fill().map((_, i) => i);
-    let columns = Array(8).fill().map((_, i) => i);
+    let seatId = 0;
 
-    for (let j =0 ; j < current.length; ++j) {
+    for (let j = 0 ; j < current.length; ++j) {
       if (current[j] === 'B') {
-        rows = rows.slice(rows.length / 2);
-      } else if (current[j] === 'F') {
-        rows = rows.slice(0, rows.length /2);
+        seatId += ROW_SIZE * 2 ** (6 - j);
       } else if (current[j] === 'R') {
-        columns = columns.slice(columns.length /2)
-      } else if (current[j] === 'L') {
-        columns = columns.slice(0, columns.length /2)
+        seatId += 2 ** (9 - j);
       }
     }
 
-    const seatId = rows[0] * 8 + columns[0];
-
     maxSeatId = Math.max(maxSeatId, seatId);
+    minSeatId = Math.min(minSeatId, seatId);
+    seats[seatId] = true;
+  }
 
-    if (allSeats.indexOf(seatId) >= 0) {
-      allSeats[allSeats.indexOf(seatId)] = 0;
+  let mySeatId = 0;
+  for (let i = minSeatId; i < maxSeatId; ++i) {
+    if (!seats[i]) {
+      mySeatId = i;
+      break
     }
   }
 
   console.log('Max seat ID:', maxSeatId);
-  console.log('My seat ID:', allSeats.filter(i => i && allSeats[i] - allSeats[i - 1] > 1)[0])
+  console.log('My seat ID:', mySeatId)
 }
 
 main().catch(console.error);
