c45ef4e7c08017ead80d39b50b824c9768568281
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-21 12:22:59 +0200

    Prettify template

--- template/index.js
+++ template/index.js
@@ -2,16 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 async function main() {
-  const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8')
-  const arr = content.split(/\n/g).filter(i => i).map(raw => {
-    return {
-      raw
-    }
-  });
+  const content = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
+  const arr = content
+    .split(/\n/g)
+    .filter((i) => i)
+    .map((raw) => {
+      return {
+        raw,
+      };
+    });
 
   let res = 0;
-
-
 
   console.log(res);
 }
