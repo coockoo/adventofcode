6673033aa1643722261b6289deee2237c763a4fb
Oleksandr Bondarchuk <oleksandrb@wix.com>
2020-12-23 13:06:48 +0200

    Add day 16 solution part 2

--- 16/index.js
+++ 16/index.js
@@ -49,24 +49,89 @@ async function main() {
 
   let errorRate = 0;
 
-  const validTickets = [];
-
-  const ranges = Object.values(criteria).flat();
-  for (let i = 0; i < tickets.length; ++i) {
-    const ticket = tickets[i];
-
-    for (let j = 0; j < ticket.length; ++j) {
-      const value = ticket[j];
-      const range = ranges.find(([from, to]) => {
-        return value >= from && value <= to;
-      });
-      if (!range) {
+  tickets.forEach((ticket) => {
+    ticket.forEach((value) => {
+      const isValid = isValidValue(value, criteria);
+      if (!isValid) {
         errorRate += value;
       }
-    }
-  }
+    });
+  });
 
   console.log('Part 1: %d', errorRate);
+
+  const validTickets = [];
+  tickets.forEach((ticket) => {
+    const isValid = isValidTicket(ticket, criteria);
+    if (isValid) {
+      validTickets.push(ticket);
+    }
+  });
+
+  console.log('There are %d valid tickets', validTickets.length);
+
+  const counts = {};
+  Object.keys(criteria).forEach((key) => {
+    counts[key] = {};
+  });
+
+  validTickets.forEach((ticket) => {
+    ticket.forEach((value, index) => {
+      Object.entries(criteria).some(([key, ranges]) => {
+        const isIn = isInRange(value, ranges);
+        if (isIn) {
+          counts[key][index] = (counts[key][index] || 0) + 1;
+        }
+      });
+    });
+  });
+
+  const finalCounts = {};
+  while (Object.keys(counts).length > 0) {
+    Object.entries(counts).forEach(([key, iCounts]) => {
+      let maxCount = 0;
+      let oneIdx = null;
+      Object.entries(iCounts).forEach(([idx, count]) => {
+        if (count === validTickets.length) {
+          maxCount += 1;
+          oneIdx = idx;
+        }
+      });
+
+      if (maxCount === 1) {
+        finalCounts[key] = oneIdx;
+        Object.keys(counts).forEach((k) => {
+          delete counts[k][oneIdx];
+        });
+        delete counts[key];
+      }
+    });
+  }
+
+  let res = 1;
+  Object.entries(finalCounts).forEach(([key, idx]) => {
+    if (key.match(/^departure /)) {
+      res *= myTicket[idx];
+    }
+  });
+
+  console.log('Part 2: %d', res);
+}
+
+function isValidTicket(ticket, criteria) {
+  return ticket.every((value) => isValidValue(value, criteria));
+}
+
+function isValidValue(value, criteria) {
+  return Object.entries(criteria).some(([_, ranges]) => {
+    return isInRange(value, ranges);
+  });
+}
+
+function isInRange(value, ranges) {
+  return ranges.some(([from, to]) => {
+    return value >= from && value <= to;
+  });
 }
 
 main().catch(console.error);
