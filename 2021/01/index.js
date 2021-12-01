533962a43301bdf71f86ff426b2d01064ca3ac38
Oleksandr Bondarchuk <oleksandrb@wix.com>
2021-12-01 07:47:00 +0200

    prettify 2021

--- 2021/01/index.js
+++ 2021/01/index.js
@@ -1,30 +1,30 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), { encoding: 'utf8' })
-const arr = input.split(/\n/).map(i => +i)
+const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), { encoding: 'utf8' });
+const arr = input.split(/\n/).map((i) => +i);
 
 module.exports.part1 = () => {
   let res = 0;
   for (let i = 1; i < arr.length; ++i) {
     if (arr[i] > arr[i - 1]) {
-      res += 1
+      res += 1;
     }
   }
-  return res
-}
+  return res;
+};
 
 module.exports.part2 = () => {
   let res = 0;
-  let window = arr[0] + arr[1] + arr[2]
-  for (let i = 3; i< arr.length; ++i) {
-    const newWindow = window + arr[i] - arr[i - 3]
+  let window = arr[0] + arr[1] + arr[2];
+  for (let i = 3; i < arr.length; ++i) {
+    const newWindow = window + arr[i] - arr[i - 3];
     if (newWindow > window) {
-      res += 1
+      res += 1;
     }
-    window = newWindow
+    window = newWindow;
   }
-  return res
-}
+  return res;
+};
 
-console.log(module.exports.part1())
+console.log(module.exports.part1());
