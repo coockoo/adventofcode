3dfac17583caff504283173f1fbc910107f46985
Oleksandr Bondarchuk <oleksandrb@wix.com>
2021-12-02 07:30:41 +0200

    add day 2

---
+++ 2021/02/index.js
@@ -0,0 +1,37 @@
+module.exports.part1 = (rows) => {
+  let position = 0;
+  let depth = 0;
+  for (let i = 0; i < rows.length; ++i) {
+    const [direction, amount] = rows[i].split(' ');
+    if (direction === 'forward') {
+      position += +amount;
+    }
+    if (direction === 'up') {
+      depth -= +amount;
+    }
+    if (direction === 'down') {
+      depth += +amount;
+    }
+  }
+  return position * depth;
+};
+
+module.exports.part2 = (rows) => {
+  let position = 0;
+  let depth = 0;
+  let aim = 0;
+  for (let i = 0; i < rows.length; ++i) {
+    const [direction, amount] = rows[i].split(' ');
+    if (direction === 'forward') {
+      position += +amount;
+      depth += +amount * aim;
+    }
+    if (direction === 'up') {
+      aim -= +amount;
+    }
+    if (direction === 'down') {
+      aim += +amount;
+    }
+  }
+  return position * depth;
+};
