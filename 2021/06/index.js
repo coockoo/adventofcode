9d20fba83f2a3a56242b1baa5fb24e120f54d755
Oleksandr Bondarchuk <oleksandrb@wix.com>
2021-12-06 07:20:10 +0200

    add solution to day 08

---
+++ 2021/06/index.js
@@ -0,0 +1,32 @@
+const { Reducer } = require('../../tools');
+
+const solve = (rows, iterations) => {
+  const arr = rows[0].split(',').map(Number);
+  const state = arr.reduce((acc, item) => {
+    if (!acc[item]) {
+      acc[item] = 0;
+    }
+    acc[item] += 1;
+    return acc;
+  }, {});
+
+  for (let i = 0; i < iterations; ++i) {
+    const t = state[0] || 0;
+    for (let j = 1; j <= 8; ++j) {
+      state[j - 1] = state[j] || 0;
+    }
+    state[8] = t || 0;
+    state[6] += t || 0;
+  }
+
+  const res = Reducer.sumOfItems(Object.values(state));
+  return res;
+};
+
+module.exports.part1 = (rows) => {
+  return solve(rows, 80);
+};
+
+module.exports.part2 = (rows) => {
+  return solve(rows, 256);
+};
