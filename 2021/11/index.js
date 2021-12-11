b6f843fbdd8d3a23822b309c3045790ea2dd4aca
Oleksandr Bondarchuk <oleksandrb@wix.com>
2021-12-11 11:29:43 +0200

    cleanup day 11

--- 2021/11/index.js
+++ 2021/11/index.js
@@ -1,57 +1,49 @@
-import { Matrix, Reducer } from '../../tools/index.js';
+import { Matrix2 } from '../../tools/index.js';
 
-const flash = (arr, i, j, flashed) => {
+const SIZE = 10;
+
+const flash = (matrix, i, j, flashed) => {
   if (flashed[`${i}:${j}`]) {
     return 0;
   }
 
-  if (arr[i]?.[j] === undefined) {
+  if (!matrix.isIn(i, j)) {
     return 0;
   }
 
-  if (arr[i][j] <= 9) {
+  if (matrix.getItem(i, j) <= 9) {
     return 0;
   }
 
   flashed[`${i}:${j}`] = true;
-  arr[i][j] = 0;
+  matrix.setItem(i, j, 0);
 
-  arr?.[i - 1]?.[j - 1] !== undefined && !flashed[`${i - 1}:${j - 1}`] && (arr[i - 1][j - 1] += 1);
-  arr?.[i - 1]?.[j] !== undefined && !flashed[`${i - 1}:${j}`] && (arr[i - 1][j] += 1);
-  arr?.[i - 1]?.[j + 1] !== undefined && !flashed[`${i - 1}:${j + 1}`] && (arr[i - 1][j + 1] += 1);
-  arr?.[i]?.[j - 1] !== undefined && !flashed[`${i}:${j - 1}`] && (arr[i][j - 1] += 1);
-  arr?.[i]?.[j + 1] !== undefined && !flashed[`${i}:${j + 1}`] && (arr[i][j + 1] += 1);
-  arr?.[i + 1]?.[j - 1] !== undefined && !flashed[`${i + 1}:${j - 1}`] && (arr[i + 1][j - 1] += 1);
-  arr?.[i + 1]?.[j] !== undefined && !flashed[`${i + 1}:${j}`] && (arr[i + 1][j] += 1);
-  arr?.[i + 1]?.[j + 1] !== undefined && !flashed[`${i + 1}:${j + 1}`] && (arr[i + 1][j + 1] += 1);
+  Matrix2.NEIGHBOURS_8.forEach(([di, dj]) => {
+    if (!flashed[`${i + di}:${j + dj}`]) {
+      matrix.incItem(i + di, j + dj);
+    }
+  });
 
   return 1;
 };
 
 export const part1 = (rows) => {
+  const matrix = Matrix2.fromNumeric(rows);
+
   let res = 0;
-  let arr = [];
-  for (let i = 0; i < rows.length; ++i) {
-    let row = rows[i];
-    if (row.length) {
-      arr.push(row.split('').map((i) => +i));
-    }
-  }
-  for (let c = 0; c < 100; ++c) {
+  let iterationsCount = 100;
+
+  for (let c = 0; c < iterationsCount; ++c) {
     let flashed = {};
     let localres = 0;
 
-    for (let i = 0; i < 10; ++i) {
-      for (let j = 0; j < 10; ++j) {
-        arr[i][j] += 1;
-      }
-    }
+    matrix.incAll();
 
     do {
       localres = 0;
-      for (let i = 0; i < 10; ++i) {
-        for (let j = 0; j < 10; ++j) {
-          localres += flash(arr, i, j, flashed);
+      for (let i = 0; i < SIZE; ++i) {
+        for (let j = 0; j < SIZE; ++j) {
+          localres += flash(matrix, i, j, flashed);
         }
       }
       res += localres;
@@ -61,36 +53,30 @@ export const part1 = (rows) => {
 };
 
 export const part2 = (rows) => {
-  let res = 0;
-  let arr = [];
-  for (let i = 0; i < rows.length; ++i) {
-    let row = rows[i];
-    if (row.length) {
-      arr.push(row.split('').map((i) => +i));
-    }
-  }
-  for (let c = 0; c < 100000; ++c) {
-    let flashed = {};
+  const matrix = Matrix2.fromNumeric(rows);
+
+  let iteration = 0;
+
+  while (true) {
+    iteration += 1;
+    const flashed = {};
+
+    matrix.incAll();
+
     let localres = 0;
-
-    for (let i = 0; i < 10; ++i) {
-      for (let j = 0; j < 10; ++j) {
-        arr[i][j] += 1;
-      }
-    }
-
     do {
       localres = 0;
-      for (let i = 0; i < 10; ++i) {
-        for (let j = 0; j < 10; ++j) {
-          localres += flash(arr, i, j, flashed);
+      for (let i = 0; i < SIZE; ++i) {
+        for (let j = 0; j < SIZE; ++j) {
+          localres += flash(matrix, i, j, flashed);
         }
       }
     } while (localres > 0);
 
-    if (arr.every((row) => row.every((item) => item === 0))) {
-      return c;
+    if (matrix.getItems().every((i) => i === 0)) {
+      break;
     }
   }
-  return res;
+
+  return iteration;
 };
