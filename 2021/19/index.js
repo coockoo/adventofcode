fd4976ea95cb0938a4f23c08ff202bf5411eb54c
Oleksandr Bondarchuk <oleksandrb@wix.com>
2021-12-19 18:37:37 +0200

    cleanup day 19 a bit

--- 2021/19/index.js
+++ 2021/19/index.js
@@ -5,6 +5,10 @@ const getKey = (point) => {
   return `${x},${y},${z}`;
 };
 
+const fromKey = (key) => {
+  return key.split(',').map((i) => +i);
+};
+
 const rotateX = ([x, y, z], times) => {
   if (times === 0) {
     return [x, y, z];
@@ -33,7 +37,7 @@ const getRotations = (point) => {
       }
     }
   }
-  return Array.from(new Set(points.map(getKey))).map((i) => i.split(',').map((i) => +i));
+  return Array.from(new Set(points.map(getKey))).map(fromKey);
 };
 
 const getScannerRotations = (scanner) => {
@@ -66,7 +70,7 @@ const parseInput = (rows) => {
       scanner = [];
       continue;
     }
-    scanner.push(row.split(',').map((i) => +i));
+    scanner.push(fromKey(row));
   }
   scanners.push(scanner);
   return scanners;
@@ -101,90 +105,61 @@ const getRef = (absScanner, refScanner) => {
     }
   }
   const ref = Object.keys(acc).filter((i) => acc[i].length >= 12)[0];
-  return ref && ref.split(',').map((i) => +i);
+  return ref && fromKey(ref);
+};
+
+const solve = (rows) => {
+  const scanners = parseInput(rows);
+  let refs = { 0: [0, 0, 0] };
+  let beacons = [scanners[0]];
+
+  while (Object.keys(refs).length !== scanners.length) {
+    console.log(Object.keys(refs).length, '/', scanners.length);
+    for (let i = 1; i < scanners.length; ++i) {
+      if (refs[i]) {
+        continue;
+      }
+      const scanner = scanners[i];
+
+      const scannerRotations = getScannerRotations(scanner);
+
+      let ref;
+      let scannerRotation;
+      for (let j = 0; j < scannerRotations.length; ++j) {
+        scannerRotation = scannerRotations[j];
+        for (let k = 0; k < beacons.length; ++k) {
+          ref = getRef(beacons[k], scannerRotation);
+          if (ref) {
+            break;
+          }
+        }
+        if (ref) {
+          break;
+        }
+      }
+      if (ref) {
+        refs[i] = ref;
+        beacons.push(scannerRotation.map((p) => toAbsolute(p, ref)));
+      }
+    }
+  }
+
+  const allScanners = Object.values(refs);
+  const allBeacons = Array.from(new Set(beacons.flatMap((i) => i).map(getKey)));
+  return [allScanners, allBeacons];
 };
 
 export const part1 = (rows) => {
-  const scanners = parseInput(rows);
-  let refs = { 0: [0, 0, 0] };
-  let abs = [scanners[0]];
-
-  while (Object.keys(refs).length !== scanners.length) {
-    console.log(Object.keys(refs).length, '/', scanners.length);
-    for (let i = 1; i < scanners.length; ++i) {
-      if (refs[i]) {
-        continue;
-      }
-      const scanner = scanners[i];
-
-      const scannerRotations = getScannerRotations(scanner);
-
-      let ref;
-      let scannerRotation;
-      for (let j = 0; j < scannerRotations.length; ++j) {
-        scannerRotation = scannerRotations[j];
-        for (let k = 0; k < abs.length; ++k) {
-          ref = getRef(abs[k], scannerRotation, k);
-          if (ref) {
-            break;
-          }
-        }
-        if (ref) {
-          break;
-        }
-      }
-      if (ref) {
-        refs[i] = ref;
-        abs.push(scannerRotation.map((p) => toAbsolute(p, ref)));
-      }
-    }
-  }
-
-  const s = new Set(abs.flatMap((i) => i).map(getKey));
-  return s.size;
+  const [, beacons] = solve(rows);
+  return beacons.length;
 };
 
 export const part2 = (rows) => {
-  const scanners = parseInput(rows);
-  let refs = { 0: [0, 0, 0] };
-  let abs = [scanners[0]];
-
-  while (Object.keys(refs).length !== scanners.length) {
-    console.log(Object.keys(refs).length, '/', scanners.length);
-    for (let i = 1; i < scanners.length; ++i) {
-      if (refs[i]) {
-        continue;
-      }
-      const scanner = scanners[i];
-
-      const scannerRotations = getScannerRotations(scanner);
-
-      let ref;
-      let scannerRotation;
-      for (let j = 0; j < scannerRotations.length; ++j) {
-        scannerRotation = scannerRotations[j];
-        for (let k = 0; k < abs.length; ++k) {
-          ref = getRef(abs[k], scannerRotation, k);
-          if (ref) {
-            break;
-          }
-        }
-        if (ref) {
-          break;
-        }
-      }
-      if (ref) {
-        refs[i] = ref;
-        abs.push(scannerRotation.map((p) => toAbsolute(p, ref)));
-      }
-    }
-  }
-
+  const [scanners] = solve(rows);
   let max = -1;
-  const values = Object.values(refs);
-  for (let i = 0; i < values.length; ++i) {
-    for (let j = 0; j < values.length; ++j) {
-      max = Math.max(max, distance(values[i], values[j]));
+  for (let i = 0; i < scanners.length; ++i) {
+    for (let j = 0; j < scanners.length; ++j) {
+      max = Math.max(max, distance(scanners[i], scanners[j]));
     }
   }
   return max;
