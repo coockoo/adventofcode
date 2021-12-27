3fe488dabf9c2f516a9beb7da226540c3fc14be0
Oleksandr Bondarchuk <oleksandrb@wix.com>
2021-12-27 23:39:07 +0200

    add both days solution

--- 2021/23/index.js
+++ 2021/23/index.js
@@ -1,349 +1,239 @@
 import { Matrix, Matrix2, Reducer } from '../../tools/index.js';
 
-let energy = {
+const LETTERS = ['A', 'B', 'C', 'D'];
+const HALLS_COUNT = 7;
+const ENERGY = {
   A: 1,
   B: 10,
   C: 100,
   D: 1000,
 };
-
-// h1, h2, h3, h4, h5, h6, h7
-//       a1  b1  c1  d1
-//       a2  b2  c2  d2
-
-const edges = [
-  { f: 'h1', t: 'h2', d: 1 },
-  //
-  { f: 'h2', t: 'h1', d: 1 },
-  { f: 'h2', t: 'h3', d: 2 },
-  { f: 'h2', t: 'a1', d: 2 },
-  //
-  { f: 'h3', t: 'h2', d: 2 },
-  { f: 'h3', t: 'h4', d: 2 },
-  { f: 'h3', t: 'a1', d: 2 },
-  { f: 'h3', t: 'b1', d: 2 },
-  //
-  { f: 'h4', t: 'h3', d: 2 },
-  { f: 'h4', t: 'h5', d: 2 },
-  { f: 'h4', t: 'b1', d: 2 },
-  { f: 'h4', t: 'c1', d: 2 },
-  //
-  { f: 'h5', t: 'h4', d: 2 },
-  { f: 'h5', t: 'h6', d: 2 },
-  { f: 'h5', t: 'c1', d: 2 },
-  { f: 'h5', t: 'd1', d: 2 },
-  { f: 'h6', t: 'h5', d: 2 },
-  { f: 'h6', t: 'h7', d: 1 },
-  { f: 'h6', t: 'd1', d: 2 },
-  //
-  { f: 'h7', t: 'h6', d: 1 },
-  //
-
-  { f: 'a1', t: 'h2', d: 2 },
-  { f: 'a1', t: 'h3', d: 2 },
-  { f: 'a1', t: 'a2', d: 1 },
-  //
-  { f: 'a2', t: 'a1', d: 1 },
-  //
-  { f: 'b1', t: 'h3', d: 2 },
-  { f: 'b1', t: 'h4', d: 2 },
-  { f: 'b1', t: 'b2', d: 1 },
-  //
-  { f: 'b2', t: 'b1', d: 1 },
-  //
-  { f: 'c1', t: 'h4', d: 2 },
-  { f: 'c1', t: 'h5', d: 2 },
-  { f: 'c1', t: 'c2', d: 1 },
-  //
-  { f: 'c2', t: 'c1', d: 1 },
-  //
-  { f: 'd1', t: 'h5', d: 2 },
-  { f: 'd1', t: 'h6', d: 2 },
-  { f: 'd1', t: 'd2', d: 1 },
-  //
-  { f: 'd2', t: 'd1', d: 1 },
-];
-
-const createState = (a1, b1, c1, d1, a2, b2, c2, d2) => ({
-  energy: 0,
-  score: Infinity,
-  h1: {
-    id: 'h1',
-    value: null,
-  },
-  h2: {
-    id: 'h2',
-    value: null,
-  },
-  h3: {
-    id: 'h3',
-    value: null,
-  },
-  h4: {
-    id: 'h4',
-    value: null,
-  },
-  h5: {
-    id: 'h5',
-    value: null,
-  },
-  h6: {
-    id: 'h6',
-    value: null,
-  },
-  h7: {
-    id: 'h7',
-    value: null,
-  },
-  a1: {
-    id: 'a1',
-    value: a1,
-    only: 'A',
-  },
-  a2: {
-    id: 'a2',
-    value: a2,
-    only: 'A',
-  },
-  b1: {
-    id: 'b1',
-    value: b1,
-    only: 'B',
-  },
-  b2: {
-    id: 'b2',
-    value: b2,
-    only: 'B',
-  },
-  c1: {
-    id: 'c1',
-    value: c1,
-    only: 'C',
-  },
-  c2: {
-    id: 'c2',
-    value: c2,
-    only: 'C',
-  },
-  d1: {
-    id: 'd1',
-    value: d1,
-    only: 'D',
-  },
-  d2: {
-    id: 'd2',
-    value: d2,
-    only: 'D',
-  },
-});
-
-const hall = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7'];
-const apt = ['a1', 'a2', 'b1', 'b2', 'c1', 'c2', 'd1', 'd2'];
-const shallow = ['a1', 'b1', 'c1', 'd1'];
-const deep = ['a2', 'b2', 'c2', 'd2'];
-
-const canMove = (state, edge) => {
-  const nf = state[edge.f];
-  const nt = state[edge.t];
-
-  if (!nf.value) {
-    return false;
-  }
-
-  if (nt.value) {
-    return false;
-  }
-
-  if (hall.includes(edge.f) && apt.includes(edge.t) && nt.only !== nf.value) {
-    return false;
-  }
-
-  if (deep.includes(edge.f) && nf.value === nf.only) {
-    return false;
-  }
-
-  const getApt = (pos) => pos.match(/^([a-z])/)[1];
-  const isAptOnPlace = (a) =>
-    [1, 2, 3, 4].every((i) => {
-      const key = `${a}${i}`;
-      return !state[key] || state[key].only === state[key].value;
-    });
-
-  if (apt.includes(edge.f) && isAptOnPlace(getApt(edge.f))) {
-    return false;
-  }
-
-  const isOnPlace = (id) => state[id].only === state[id].value;
-  const isFromShallow = shallow.includes(edge.f);
-  const isFromDeep = deep.includes(edge.f);
-  const isToDeep = deep.includes(edge.t);
-
-  if (isFromDeep && isOnPlace(edge.f)) {
-    return false;
-  }
-
-  if (isFromShallow && isToDeep && !isOnPlace(edge.f)) {
-    return false;
-  }
-
-  if (nf.value === 'D' && ['h1', 'h2', 'h7'].includes(edge.t)) {
-    return false;
-  }
-
-  return true;
+const DISTANCES = {
+  0: { A: 3, B: 5, C: 7, D: 9, 0: 0, 1: 1, 2: 3, 3: 5, 4: 7, 5: 9, 6: 10 },
+  1: { A: 2, B: 4, C: 6, D: 8, 0: 1, 1: 0, 2: 2, 3: 4, 4: 6, 5: 8, 6: 9 },
+  2: { A: 2, B: 2, C: 4, D: 6, 0: 3, 1: 2, 2: 0, 3: 2, 4: 4, 5: 6, 6: 7 },
+  3: { A: 4, B: 2, C: 2, D: 4, 0: 5, 1: 4, 2: 2, 3: 0, 4: 2, 5: 4, 6: 5 },
+  4: { A: 6, B: 4, C: 2, D: 2, 0: 7, 1: 6, 2: 4, 3: 2, 4: 0, 5: 2, 6: 3 },
+  5: { A: 8, B: 6, C: 4, D: 2, 0: 9, 1: 8, 2: 6, 3: 4, 4: 2, 5: 0, 6: 1 },
+  6: { A: 9, B: 7, C: 5, D: 3, 0: 10, 1: 9, 2: 7, 3: 5, 4: 3, 5: 1, 6: 0 },
+  A: { 0: 3, 1: 2, 2: 2, 3: 4, 4: 6, 5: 8, 6: 9 },
+  B: { 0: 5, 1: 4, 2: 2, 3: 2, 4: 4, 5: 6, 6: 7 },
+  C: { 0: 7, 1: 6, 2: 4, 3: 2, 4: 2, 5: 4, 6: 5 },
+  D: { 0: 9, 1: 8, 2: 6, 3: 4, 4: 2, 5: 2, 6: 3 },
 };
 
-const move = (state, node, edge) => {
-  const { value } = node;
-  let newState = {
-    ...state,
-    energy: state.energy + energy[value] * edge.d,
-    [node.id]: {
-      ...state[node.id],
-      value: null,
-    },
-    [edge.t]: {
-      ...state[edge.t],
-      value,
-    },
-  };
-  newState.score = getScore(newState);
-  return newState;
-};
-
-const getScore = (state) => {
-  let score = 0;
-  Object.entries(state).forEach(([key, node]) => {
-    if (node.only && node.only !== node.value) {
-      score += 1;
-    }
-    if (!node.only && node.value) {
-      score += 1;
-    }
-
-    if (node.value === 'D') {
-      if (key === 'h5' || key === 'h6') {
-        score += 1;
-      }
-      if (key === 'h4' || key === 'h7' || key == 'c1') {
-        score += 2;
-      }
-      if (key === 'h3' || key === 'c2' || key === 'b1') {
-        score += 3;
-      }
-      if (key === 'h2' || key === 'b2' || key === 'a1') {
-        score += 4;
-      }
-      if (key === 'h1' || key === 'a2') {
-        score += 5;
-      }
-    }
-
-    if (node.value === 'C') {
-      if (key === 'h4' || key === 'h5') {
-        score += 1;
-      }
-      if (key === 'h3' || key === 'h6' || key === 'b1' || key === 'd1') {
-        score += 2;
-      }
-      if (key === 'h2' || key === 'h7' || key === 'b2' || key === 'd2' || key === 'a1') {
-        score += 3;
-      }
-      if (key === 'h1' || key === 'a2') {
-        score += 4;
-      }
-    }
-
-    if (node.value === 'B') {
-      if (key === 'h3' || key === 'h4') {
-        score += 1;
-      }
-
-      if (key === 'h2' || key === 'h5' || key === 'a1' || key === 'c1') {
-        score += 2;
-      }
-
-      if (key === 'h1' || key === 'h6' || key === 'a2' || key === 'c2' || key === 'd1') {
-        score += 3;
-      }
-
-      if (key === 'd2' || key === 'h7') {
-        score += 4;
-      }
-    }
-
-    if (node.value === 'A') {
-      if (key === 'h2' || key === 'h3') {
-        score += 1;
-      }
-      if (key === 'h4' || key === 'h1' || key == 'b1') {
-        score += 2;
-      }
-      if (key === 'h5' || key === 'b2' || key === 'c1') {
-        score += 3;
-      }
-      if (key === 'h6' || key === 'c2' || key === 'd1') {
-        score += 4;
-      }
-      if (key === 'h7' || key === 'd2') {
-        score += 5;
-      }
-    }
-  });
-  return score;
-};
-
-const getKey = (state) => {
-  return ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'a1', 'a2', 'b1', 'b2', 'c1', 'c2', 'd1', 'd2']
-    .map((k) => state[k].value || '.')
-    .join(';');
-};
-
-export const part1 = (rows) => {
-  let values = rows
+const parseInput = (rows) =>
+  rows
     .join('')
     .split('')
-    .filter((i) => ['A', 'B', 'C', 'D'].includes(i));
-  const initialState = createState(...values);
-  let states = [initialState];
+    .filter((i) => LETTERS.includes(i));
 
-  let res = {};
+const createState = (values) => {
+  let state = {
+    energy: 0,
+    H: new Array(HALLS_COUNT).fill(null),
+  };
+  let count = values.length / LETTERS.length;
+  for (let i = 1; i <= count; ++i) {
+    for (const letter of LETTERS) {
+      if (!state[letter]) {
+        state[letter] = [];
+      }
+      state[letter].push(values.shift());
+    }
+  }
+  return state;
+};
+
+const toString = (state, depth) => {
+  let res = ['#############'];
+
+  let hall = [];
+  hall.push('#');
+  for (let i = 0; i < state.H.length; ++i) {
+    const t = state.H[i] || '.';
+    hall.push(t);
+    if (i > 0 && i < HALLS_COUNT - 2) {
+      hall.push('.');
+    }
+  }
+  hall.push('#');
+  res.push(hall.join(''));
+
+  for (let i = 0; i < depth; ++i) {
+    let room = [];
+    if (i === 0) {
+      room.push('###');
+    } else {
+      room.push('  #');
+    }
+    for (const letter of LETTERS) {
+      const t = state[letter].join('').padStart(depth, '.').split('');
+      room.push(t[i]);
+      room.push('#');
+    }
+    if (i === 0) {
+      room.push('##');
+    } else {
+      room.push('  ');
+    }
+    res.push(room.join(''));
+  }
+  res.push('  #########  ');
+  return res.join('\n');
+};
+
+const canGetTo = (state, fromRoom, fromPos, toRoom, toPos) => {
+  let from = fromPos;
+  if (fromRoom === 'A') {
+    from = toPos < 1 ? 1 : 2;
+  }
+  if (fromRoom === 'B') {
+    from = toPos < 3 ? 2 : 3;
+  }
+  if (fromRoom === 'C') {
+    from = toPos < 4 ? 3 : 4;
+  }
+  if (fromRoom === 'D') {
+    from = toPos < 5 ? 4 : 5;
+  }
+
+  let to = toPos;
+  if (toRoom === 'A') {
+    to = fromPos < 1 ? 1 : 2;
+  }
+  if (toRoom === 'B') {
+    to = fromPos < 3 ? 2 : 3;
+  }
+  if (toRoom === 'C') {
+    to = fromPos < 4 ? 3 : 4;
+  }
+  if (toRoom === 'D') {
+    to = fromPos < 5 ? 4 : 5;
+  }
+
+  let count = fromRoom === 'H' ? -1 : 0;
+  for (let i = Math.min(from, to); i <= Math.max(from, to); ++i) {
+    if (state.H[i]) {
+      count += 1;
+    }
+  }
+  const res = count === 0;
+
+  return res;
+};
+
+const move = (state, room, pos, depth) => {
+  let value = room === 'H' ? state[room][pos] : state[room][0];
+
+  if (!value) {
+    return [];
+  }
+
+  if (room === 'H') {
+    if (!canGetTo(state, room, pos, value, undefined)) {
+      return [];
+    }
+
+    const canEnter = state[value].every((item) => item === value);
+    if (!canEnter) {
+      return [];
+    }
+
+    const distance = DISTANCES[pos][value] + depth - 1 - state[value].length;
+
+    return [
+      {
+        ...state,
+        energy: state.energy + ENERGY[value] * distance,
+        H: state.H.map((item, idx) => (idx === pos ? null : item)),
+        [value]: [...state[value], value],
+      },
+    ];
+  }
+
+  const isOnPlace = state[room].every((item) => item === room);
+  if (isOnPlace) {
+    return [];
+  }
+
+  let states = [];
+  for (let i = 0; i < HALLS_COUNT; ++i) {
+    if (!canGetTo(state, room, undefined, 'H', i)) {
+      continue;
+    }
+
+    let roomDistance = depth - state[room].length;
+    let hallDistance = DISTANCES[room][i];
+
+    states.push({
+      ...state,
+      energy: state.energy + (roomDistance + hallDistance) * ENERGY[value],
+      H: state.H.map((item, index) => (index === i ? value : item)),
+      [room]: state[room].slice(1),
+    });
+  }
+  return states;
+};
+
+const isWin = (state) => {
+  return (
+    state.H.every((i) => !i) &&
+    state.A.every((i) => i === 'A') &&
+    state.B.every((i) => i === 'B') &&
+    state.C.every((i) => i === 'C') &&
+    state.D.every((i) => i === 'D')
+  );
+};
+
+const getKey = (state, depth) => {
+  return [
+    ...state.H.map((i) => i || '.'),
+    ...state.A.join('').padStart(depth, '.'),
+    ...state.B.join('').padStart(depth, '.'),
+    ...state.C.join('').padStart(depth, '.'),
+    ...state.D.join('').padStart(depth, '.'),
+  ].join('');
+};
+
+const solve = (rows, extraStr = '') => {
+  const rawValues = parseInput(rows);
+  let extra = extraStr.split('');
+  const values = [...rawValues.slice(0, LETTERS.length), ...extra, ...rawValues.slice(LETTERS.length)];
+  const depth = values.length / LETTERS.length;
+  const initialState = createState(values);
+  let states = [initialState];
   let minEnergy = Infinity;
+  let seen = {};
 
   while (states.length) {
     const state = states.pop();
-    const key = getKey(state);
-    console.log(states.length, state.score, state.energy);
-    if (res[key]) {
+    console.log(states.length, state.energy);
+    const key = getKey(state, depth);
+    if (seen[key]) {
       continue;
     }
-    res[key] = true;
     if (state.energy > minEnergy) {
       break;
     }
-    for (let i = 0; i < edges.length; ++i) {
-      const e = edges[i];
-      const node = state[e.f];
-      if (!canMove(state, e)) {
-        continue;
-      }
-      let newState = move(state, node, e);
-      if (newState.score === 0) {
-        if (newState.energy < minEnergy) {
-          minEnergy = newState.energy;
-        }
-        continue;
-      }
-      states.push(newState);
+
+    if (isWin(state)) {
+      minEnergy = Math.min(minEnergy, state.energy);
     }
+    seen[key] = true;
+    const newStates = [];
+    for (const letter of LETTERS) {
+      newStates.push(...move(state, letter, undefined, depth));
+    }
+    for (let i = 0; i < HALLS_COUNT; ++i) {
+      newStates.push(...move(state, 'H', i, depth));
+    }
+    states.push(...newStates);
     states = states.sort((a, b) => b.energy - a.energy);
   }
+
   return minEnergy;
 };
 
-export const part2 = (rows) => {
-  // const arr = rows.map(Number)
-  let res = 0;
-  // add implementation
-  return res;
-};
+export const part1 = (rows) => solve(rows);
+export const part2 = (rows) => solve(rows, 'DCBADBAC');
