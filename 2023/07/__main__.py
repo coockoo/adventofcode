44ed1874dc121f1f1e657711e7d446af1d469287
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-12-07 08:14:42 +0200

    2023-07: add solution to day 2 and cleanup a bit

--- 2023/07/__main__.py
+++ 2023/07/__main__.py
@@ -1,42 +1,80 @@
 from functools import cmp_to_key
 
 
+def get_rank_in_vals(vals):
+    if 5 in vals:
+        return 1
+    if 4 in vals:
+        return 2
+    if 3 in vals and 2 in vals:
+        return 3
+    if 3 in vals:
+        return 4
+    if vals.count(2) == 2:
+        return 5
+    if 2 in vals:
+        return 6
+    return 7
+
+
 def get_rank(hand):
     res = {}
     for i in hand[0]:
         if not res.get(i):
             res[i] = 0
         res[i] += 1
-    vals = res.values()
-    if (5 in vals):
-        return 1
-    if (4 in vals):
-        return 2
-    if (3 in vals and 2 in vals):
-        return 3
-    if (3 in vals):
-        return 4
-    if (list(vals).count(2) == 2):
-        return 5
-    if (2 in vals):
-        return 6
-    return 7
+    vals = list(res.values())
+
+    return get_rank_in_vals(vals)
+
+
+def get_rank_2(hand):
+    res = {}
+    for i in hand[0]:
+        if not res.get(i):
+            res[i] = 0
+        res[i] += 1
+    js = res.pop('J') if res.get('J') else 0
+    vals = list(res.values())
+
+    if not len(vals):
+        vals = [5]
+    else:
+        maxval = max(vals)
+        if js:
+            vals[vals.index(maxval)] += js
+
+    return get_rank_in_vals(vals)
 
 
 def get_value(card):
     return 'AKQJT98765432'.find(card)
 
 
-def sort_fn(a, b):
-    ra = get_rank(a)
-    rb = get_rank(b)
+def get_value_2(card):
+    return 'AKQT98765432J'.find(card)
+
+
+def sort_fn(a, b, rank_fn, value_fn):
+    ra = rank_fn(a)
+    rb = rank_fn(b)
     if ra == rb:
         for i in range(len(a[0])):
-            if get_value(a[0][i]) > get_value(b[0][i]):
+            va = value_fn(a[0][i])
+            vb = value_fn(b[0][i])
+            if va > vb:
                 return -1
-            if get_value(a[0][i]) < get_value(b[0][i]):
+            if va < vb:
                 return 1
-    return 1 if rb > ra else -1
+    return rb - ra
+
+
+def solve(hands, rank_fn, value_fn):
+    hands = sorted(hands, key=cmp_to_key(lambda a, b: sort_fn(a, b, rank_fn, value_fn)))
+    res = 0
+    for idx, hand in enumerate(hands):
+        res += ((idx + 1) * hand[1])
+    return res
 
 
 def main():
@@ -52,12 +90,8 @@ def main():
             cur[1] = int(cur[1])
             hands.append(cur)
 
-        hands = sorted(hands, key=cmp_to_key(sort_fn))
-
-        res = 0
-        for idx, hand in enumerate(hands):
-            res += ((idx + 1) * hand[1])
-        print('part 1', res)
+        print('part 1', solve(hands, get_rank, get_value))
+        print('part 2', solve(hands, get_rank_2, get_value_2))
 
 
 main()
