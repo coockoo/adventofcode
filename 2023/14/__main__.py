a4d6b4e911f2c044e1d8b7d0d1f564cd8f579a7e
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-12-14 22:27:57 +0200

    2023-14: disgusting python stuff

--- 2023/14/__main__.py
+++ 2023/14/__main__.py
@@ -2,30 +2,26 @@ def transpose(rows: list[str]) -> list[str]:
     return [''.join([r[i] for r in rows]) for i in range(len(rows[0]))]
 
 
+def part1(rows: list[str]) -> int:
+    res = 0
+    cols = transpose(rows)
+    for col in cols:
+        w = len(col)
+        for idx, c in enumerate(col):
+            if c == 'O':
+                res += w
+                w -= 1
+            if c == '#':
+                w = len(col) - idx - 1
+    return res
+
+
 def main():
     with open('./2023/14/input.txt', 'r', encoding='utf-8') as f:
         content = f.read()
         lines = content.split('\n')
 
-        rows = []
-        res = 0
-        for line in lines:
-            if not line:
-                continue
-            rows.append(line)
-
-        cols = transpose(rows)
-
-        for col in cols:
-            w = len(col)
-            for idx, c in enumerate(col):
-                if c == 'O':
-                    res += w
-                    w -= 1
-                if c == '#':
-                    w = len(col) - idx - 1
-
-        print('Part 1', res)
+        print('Part 1', part1(list(line for line in lines if line)))
 
 
 main()
