e3c7b4ea0041016d8e502fa22d33af9c0e79d608
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-12-15 20:43:12 +0200

    2023-12: reingeneer part 1 with look for future

--- 2023/12/__main__.py
+++ 2023/12/__main__.py
@@ -1,40 +1,78 @@
-def match(pattern: str, positions: list[int]) -> bool:
-    return list(map(lambda i: len(i), filter(lambda i: i, pattern.split('.')))) == positions
+import re
+from os import path
 
 
-def generate(i, p):
-    return bin(i).split('b')[1].zfill(p).replace('1', '#').replace('0', '.')
+def is_cool(pattern: str, len: int) -> bool:
+    my_re = rf'^[#\?]{{{len}}}([^#]|$)'
+    return re.match(my_re, pattern) is not None
 
 
-def replace(raw, repl: str):
-    repl = [*reversed(repl)]
-    res = ''
-    for i in raw:
-        if i == '?':
-            res += repl.pop()
-        else:
-            res += i
+def trim(pattern: str) -> str:
+    qidx = pattern.index('?') if '?' in pattern else None
+    hidx = pattern.index('#') if '#' in pattern else None
+    if qidx is None and hidx is None:
+        return ''
+    m = qidx if hidx is None else hidx if qidx is None else min(qidx, hidx)
+    if not m:
+        return pattern
+    return pattern[m:]
+
+
+def solve(pattern: str, positions: list[int], cache={}, depth: int = 0) -> int:
+    # pad = ''.join(' ' for i in range(depth))
+    # print(pad, 'calculating', f'"{pattern}"', positions)
+    res = 0
+
+    key = f'{pattern}:{",".join(str(p) for p in positions)}'
+    if cache.get(key):
+        # print('cache hit', key)
+        return cache.get(key)
+
+    if not len(positions):
+        if '#' in pattern:
+            # print(pad, 'no positions and hash')
+            return 0
+        # print(pad, 'no positions no hash')
+        return 1
+    if not len(pattern):
+        # print(pad, 'empty pattern')
+        return 0
+
+    if is_cool(pattern, positions[0]):
+        # print(pad, 'start cool', pattern, positions[0])
+        deepres = solve(trim(pattern[positions[0] + 1:]), positions[1:], cache, depth + 1)
+        # print(pad, 'solved cool', deepres)
+        res += deepres
+    if (pattern.startswith('?')):
+        # print(pad, 'start questionmark', pattern, positions[0])
+        deepres = solve(trim(pattern[1:]), positions, cache, depth + 1)
+        # print(pad, 'solved questionmark', deepres)
+        res += deepres
+
+    cache[key] = res
+    # print(pad, 'solved', res)
     return res
 
 
 def main():
-    with open('./2023/12/input.txt', 'r', encoding='utf-8') as f:
+    inp = path.join(path.dirname(__file__), 'input.txt')
+    with open(inp, 'r', encoding='utf-8') as f:
         content = f.read()
-        lines = content.split('\n')
+        lines = list(line for line in content.split('\n') if line)
 
+        times = 1
         res = 0
-        for line in lines:
-            if not line:
-                continue
-            raw_pattern, positions = line.split(' ')
-            positions = [int(i) for i in positions.split(',')]
-            p = raw_pattern.count('?')
-            for i in range(pow(2, p)):
-                replacements = generate(i, p)
-                t = replace(raw_pattern, replacements)
-                if match(t, positions):
-                    res += 1
+        for idx, line in enumerate(lines):
+            # print(idx + 1, 'of', len(lines))
+            pattern, positions = line.split(' ')
+            pattern = '?'.join([pattern] * times)
+            pattern = trim(pattern)
+            positions = [int(i) for i in positions.split(',')] * times
+            res += solve(pattern, positions)
 
-        print('part 1', res)
+            # print(pattern, positions, solve(pattern, positions))
+
+        print('Part 1', res)
+
 
 main()
