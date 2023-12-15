dbda93ee5b90b0c2e5faebd7fa3e389f78fd529c
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-12-15 11:30:42 +0200

    2023-15: solve reading challenge for part 2

--- 2023/15/__main__.py
+++ 2023/15/__main__.py
@@ -1,4 +1,5 @@
 from os import path
+import re
 
 
 def hash(s: str) -> int:
@@ -10,15 +11,48 @@ def hash(s: str) -> int:
     return res
 
 
+def part1(cmds: list[str]) -> int:
+    return sum(hash(cmd) for cmd in cmds)
+
+
+def part2(cmds: list[str]) -> int:
+    boxes = {}
+    for cmd in cmds:
+        label = re.match(r'^\w+', cmd)[0]
+        box = hash(label)
+
+        if not boxes.get(box):
+            boxes[box] = []
+
+        if '-' in cmd:
+            boxes[box] = [i for i in boxes.get(box) if i[0] != label]
+        if '=' in cmd:
+            fl = int(cmd.split('=')[1])
+            found = False
+            for idx, i in enumerate(boxes.get(box)):
+                if i[0] == label:
+                    found = True
+                    boxes.get(box)[idx] = (label, fl)
+                    break
+            if not found:
+                boxes[box].append((label, fl))
+
+    res = 0
+    for k, v in boxes.items():
+        for idx, i in enumerate(v):
+            _, fl = i
+            res += (idx + 1) * fl * (k + 1)
+    return res
+
+
 def main():
     inp = path.join(path.dirname(__file__), 'input.txt')
     with open(inp, 'r', encoding='utf-8') as f:
         content = f.read()
         cmds = content.split('\n')[0].split(',')
 
-        res = sum(hash(cmd) for cmd in cmds)
-        print(res)
-
+        print('Part 1', part1(cmds))
+        print('Part 2', part2(cmds))
 
 
 main()
