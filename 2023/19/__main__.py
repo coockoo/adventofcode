99a7b95912096aaedb87d3fbf2bf06cb82a78856
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-12-23 23:15:14 +0200

    2023-19: solution to part 2 (dumb bug took a couple of hours)

--- 2023/19/__main__.py
+++ 2023/19/__main__.py
@@ -4,114 +4,93 @@ from os import path
 class Range:
     def __init__(self, values: dict = None):
         self.values = values or {
-            'x': None,
-            'm': None,
-            'a': None,
-            's': None,
+            "x": (1, 4000),
+            "m": (1, 4000),
+            "a": (1, 4000),
+            "s": (1, 4000),
         }
 
     def cut(self, attr: str, op: str, val: int, reverse: bool = False):
         next = self.values.copy()
-        prev = self.values.get(attr) if self.values.get(attr) else (0, 4000)
+        opp = self.values.copy()
 
-        if op == '>':
-            if reverse:
-                next[attr] = (prev[0], min(val, prev[1]))
-            else:
-                next[attr] = (max(val + 1, prev[0]), prev[1])
-        if op == '<':
-            if reverse:
-                next[attr] = (max(val, prev[0]), prev[1])
-            else:
-                next[attr] = (prev[0], min(val - 1, prev[1]))
-        return Range(next)
+        prev = self.values.get(attr)
 
-    def copy(self):
-        return Range(self.values.copy())
+        if op == ">":
+            next[attr] = (max(val + 1, prev[0]), prev[1])
+            opp[attr] = (prev[0], min(val, prev[1]))
+        if op == "<":
+            next[attr] = (prev[0], min(val - 1, prev[1]))
+            opp[attr] = (max(val, prev[0]), prev[1])
+        return Range(next), Range(opp)
 
     def __repr__(self):
-        return f'{self.values}'
+        def fmt(val: str):
+            v = self.values.get(val)
+            left = str(v[0]).rjust(4, " ")
+            right = str(v[1]).rjust(4, " ")
+            return f"{left}<={val}<={right}"
+
+        return "{" + ";".join([fmt("x"), fmt("m"), fmt("a"), fmt("s")]) + "}"
+
+    def value(self):
+        def get_v(val: str):
+            v = self.values.get(val)
+            t = v[1] - v[0] + 1
+            return t
+
+        return get_v("x") * get_v("m") * get_v("a") * get_v("s")
 
 
 def part1(parts, cmds):
     res = 0
     for part in parts:
-        current = 'in'
+        current = "in"
         while True:
-            if current == 'A':
+            if current == "A":
                 res += sum(part.values())
                 break
-            if current == 'R':
+            if current == "R":
                 break
             rules = cmds.get(current)
             for rule in rules:
                 k, op, val, to = rule
                 if k is None:
                     current = to
-                if op == '<' and part.get(k) < val:
+                if op == "<" and part.get(k) < val:
                     current = to
                     break
-                if op == '>' and part.get(k) > val:
+                if op == ">" and part.get(k) > val:
                     current = to
                     break
     return res
 
 
-def main():
-    inp = path.join(path.dirname(__file__), 'demo.txt')
-    with open(inp, 'r', encoding='utf-8') as f:
-        content = f.read()
-        lines = list(line for line in content.split('\n') if line)
+def part2(parts, cmds):
+    q = [("in", Range())]
+    res = []
+    while len(q):
+        name, opp = q.pop()
+        for cmd in cmds.get(name):
+            k, op, val, to = cmd
+            next, opp = opp.cut(k, op, val) if k else (opp, None)
+            if to == "A":
+                res.append(next)
+            elif to != "R":
+                q.append((to, next))
 
-        parts, cmds = parse(lines)
-        # print('Part 1', part1(parts, cmds))
-
-        q = [('in', Range())]
-        res = []
-        while len(q):
-            name, r = q.pop()
-            # print('name', name, r)
-            opp = r
-            for cmd in cmds.get(name):
-                # print('cmd', cmd)
-                k, op, val, to = cmd
-                if not k:
-                    if to == 'A':
-                        # print('approve no key')
-                        res.append(r)
-                    elif to == 'R':
-                        None
-                        # print('reject no key')
-                    else:
-                        # print('no kep append opp', to, opp)
-                        q.append((to, opp))
-                else:
-                    next = r.cut(k, op, val)
-                    # print('next', next)
-                    opp = opp.cut(k, op, val, True)
-                    if to == 'A':
-                        # print('approve cut')
-                        res.append(next)
-                    elif to == 'R':
-                        None
-                        # print('reject cut')
-                    else:
-                        # print('queue cut', to)
-                        q.append((to, next))
-
-        print('res', [i.values.get('x') for i in res])
-        print((1415 - 0 + 4000 - 2663) * 4000 * 4000 * 4000)
+    return sum(i.value() for i in res)
 
 
 def parse_rule(rule: str) -> tuple[str, str, int]:
-    if '<' in rule:
-        k, v = rule.split('<')
-        v, t = v.split(':')
-        return (k, '<', int(v), t)
-    if '>' in rule:
-        k, v = rule.split('>')
-        v, t = v.split(':')
-        return (k, '>', int(v), t)
+    if "<" in rule:
+        k, v = rule.split("<")
+        v, t = v.split(":")
+        return (k, "<", int(v), t)
+    if ">" in rule:
+        k, v = rule.split(">")
+        v, t = v.split(":")
+        return (k, ">", int(v), t)
     return (None, None, None, rule)
 
 
@@ -119,19 +98,30 @@ def parse(lines: list[str]) -> tuple[list[dict], dict]:
     parts = []
     cmds = {}
     for line in lines:
-        if line.startswith('{'):
-            params = line[1:-1].split(',')
+        if line.startswith("{"):
+            params = line[1:-1].split(",")
             d = {}
             for p in params:
-                key, val = p.split('=')
+                key, val = p.split("=")
                 d[key] = int(val)
             parts.append(d)
         else:
-            key, rules = line.split('{')
-            rules = rules[:-1].split(',')
+            key, rules = line.split("{")
+            rules = rules[:-1].split(",")
             rules = [parse_rule(rule) for rule in rules]
             cmds[key] = rules
     return parts, cmds
+
+
+def main():
+    inp = path.join(path.dirname(__file__), "input.txt")
+    with open(inp, "r", encoding="utf-8") as f:
+        content = f.read()
+        lines = list(line for line in content.split("\n") if line)
+
+        parts, cmds = parse(lines)
+        print("Part 1", part1(parts, cmds))
+        print("Part 2", part2(parts, cmds))
 
 
 main()
