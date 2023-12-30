c160bab5becf31c12d6058312f141f75297b636c
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-12-30 12:35:24 +0200

    2023-24: a lot more simplification

--- 2023/24/__main__.py
+++ 2023/24/__main__.py
@@ -1,4 +1,11 @@
 from os import path
+from sys import argv
+
+mode = argv[1] if len(argv) > 1 else "input"
+area_from = 200000000000000 if mode != "demo" else 7
+area_to = 400000000000000 if mode != "demo" else 27
+
+Data = tuple[int, int, int, int, int, int]
 
 
 def calc_a_b(x, y, z, dx, dy, dz) -> tuple[float, float]:
@@ -7,13 +14,16 @@ def calc_a_b(x, y, z, dx, dy, dz) -> tuple[float, float]:
     return a, b
 
 
-def in_future(x, y, z, dx, dy, dz, tx, ty) -> tuple[float, float]:
-    x_in_future = (dx > 0 and tx >= x) or (dx < 0 and tx <= x)
-    y_in_future = (dy > 0 and ty >= y) or (dy < 0 and ty <= y)
-    return x_in_future and y_in_future
+def val_in_future(v0: float, dv: float, v: float) -> bool:
+    return (dv > 0 and v > v0) or (dv < 0 and v <= v0)
 
 
-Data = tuple[int, int, int, int, int, int]
+def in_future(x, y, z, dx, dy, dz, tx, ty) -> bool:
+    return val_in_future(x, dx, tx) and val_in_future(y, dy, ty)
+
+
+def in_area(x, y) -> bool:
+    return area_from <= x <= area_to and area_from <= y <= area_to
 
 
 def parse(lines: list[str]) -> list[Data]:
@@ -26,10 +36,30 @@ def parse(lines: list[str]) -> list[Data]:
     return ps
 
 
-def part1(ps: list[Data], mode: str) -> int:
+def intersect(p1: Data, p2: Data) -> bool:
+    x1, y1, z1, dx1, dy1, dz1 = p1
+    a1, b1 = calc_a_b(*p1)
+
+    x2, y2, z2, dx2, dy2, dz2 = p2
+    a2, b2 = calc_a_b(*p2)
+
+    if a1 == a2:
+        return None
+
+    x = (b2 - b1) / (a1 - a2)
+    y = a1 * x + b1
+
+    if not in_future(*p1, x, y):
+        return None
+
+    if not in_future(*p2, x, y):
+        return None
+
+    return (x, y)
+
+
+def part1(ps: list[Data]) -> int:
     res = 0
-    fr = 200000000000000 if mode != "demo" else 7
-    to = 400000000000000 if mode != "demo" else 27
     for idx, p1 in enumerate(ps):
         x1, y1, z1, dx1, dy1, dz1 = p1
         a1, b1 = calc_a_b(*p1)
@@ -42,45 +72,34 @@ def part1(ps: list[Data], mode: str) -> int:
             x = (b2 - b1) / (a1 - a2)
             y = a1 * x + b1
 
-            is_in_area = fr <= x and x <= to and fr <= y and y <= to
-            if not is_in_area:
-                continue
-
-            if not in_future(*p1, x, y):
-                continue
-
-            if not in_future(*p2, x, y):
-                continue
-
-            res += 1
+            if in_area(x, y) and in_future(*p1, x, y) and in_future(*p2, x, y):
+                res += 1
     return res
 
 
 def main():
-    mode = "input"
     inp = path.join(path.dirname(__file__), f"{mode}.txt")
-    with open(inp, "r", encoding="utf-8") as f:
-        content = f.read()
-        lines = list(line for line in content.split("\n") if line)
+    lines = list(line for line in open(inp) if line)
 
-        ps = parse(lines)
+    ps = parse(lines)
 
-        print("Part1", part1(ps, mode))
+    print("Part 1", part1(ps))
+    return
 
-        speed_range = 500
-        for dx in range(-speed_range, speed_range + 1):
-            for dy in range(-speed_range, speed_range + 1):
-                if dx == 0:
-                    continue
+    speed_range = 500
+    for dx in range(-speed_range, speed_range + 1):
+        for dy in range(-speed_range, speed_range + 1):
+            if dx == 0:
+                continue
 
-                a = dy / dx
+            a = dy / dx
 
-                for idx, p1 in enumerate(ps):
-                    x1, y1, z1, dx1, dy1, dz1 = p1
-                    a1, b1 = calc_a_b(*p1)
-                    for p2 in ps[idx + 1:]:
-                        x2, y2, z2, dx2, dy2, dz2 = p2
-                        a2, b2 = calc_a_b(*p2)
+            for idx, p1 in enumerate(ps):
+                x1, y1, z1, dx1, dy1, dz1 = p1
+                a1, b1 = calc_a_b(*p1)
+                for p2 in ps[idx + 1:]:
+                    x2, y2, z2, dx2, dy2, dz2 = p2
+                    a2, b2 = calc_a_b(*p2)
 
 
 main()
