0435f786e4ac8cfc44d6de03e32f5d1ec400c48a
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-12-04 22:38:40 +0200

    2024-04: solution to part 1

---
+++ 2024/04/main.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const file = try std.fs.cwd().openFile("./2024/04/input.txt", .{});
+    defer file.close();
+    var bufReader = std.io.bufferedReader(file.reader());
+    var reader = bufReader.reader();
+    var buf: [4096]u8 = undefined;
+    var map = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer map.deinit();
+    var res: usize = 0;
+    var row_len: usize = 0;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        row_len = @max(row_len, line.len);
+        for (line) |c| {
+            try map.append(c);
+        }
+    }
+    for (0..map.items.len) |idx| {
+        res += check(map.items, idx, row_len);
+    }
+    std.debug.print("Part 1: {d}\n", .{res});
+}
+
+fn check(map: []const u8, idx: usize, row_len: usize) usize {
+    var res: usize = 0;
+    const y: usize = @divTrunc(idx, row_len);
+    const x: usize = @rem(idx, row_len);
+    const rows: usize = @divTrunc(map.len, row_len);
+    if (x + 4 <= row_len) {
+        const slice = makeSlice(map, idx, row_len, 1, 0);
+        if (okSlice(&slice)) {
+            res += 1;
+        }
+    }
+    if (y + 4 <= rows) {
+        const slice = makeSlice(map, idx, row_len, 0, 1);
+        if (okSlice(&slice)) {
+            res += 1;
+        }
+    }
+    if (x + 4 <= row_len and y + 4 <= rows) {
+        const slice = makeSlice(map, idx, row_len, 1, 1);
+        if (okSlice(&slice)) {
+            res += 1;
+        }
+    }
+    if (x >= 4 - 1 and y + 4 <= rows) {
+        const slice = makeSlice(map, idx, row_len, -1, 1);
+        // std.debug.print("row: {d} col: {d} slice: {s}\n", .{ y, x, slice });
+        if (okSlice(&slice)) {
+            // std.debug.print("yay!\n", .{});
+            res += 1;
+        }
+    }
+    return res;
+}
+
+fn makeSlice(map: []const u8, idx: usize, row_len: usize, dx: isize, dy: usize) [4]u8 {
+    var res: [4]u8 = undefined;
+    @memset(&res, 0);
+    inline for (0..4) |ui| {
+        const i = @as(isize, @intCast(ui));
+        const shifted_idx = @as(isize, @intCast(idx + row_len * ui * dy)) + i * dx;
+        res[i] = map[@abs(shifted_idx)];
+    }
+    return res;
+}
+
+fn okSlice(slice: []const u8) bool {
+    return std.mem.eql(u8, "XMAS", slice) or std.mem.eql(u8, "SAMX", slice);
+}
--- 2024/README.md
+++ 2024/README.md
@@ -4,3 +4,9 @@ Parameters are immutable (wow, this is cool concept). Primitive values are passe
 Structs can be passed by reference or by value (zig decides which is faster).
 
 [Docs](https://ziglang.org/documentation/master/#toc-Pass-by-value-Parameters)
+
+## Casting
+
+It sucks big time.
+
+`usize` and `isize`
