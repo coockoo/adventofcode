c1a58d70c2af8019ae64b7d89ed333c08c03a41e
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-12-04 23:10:03 +0200

    2024-04: solution to part 2

--- 2024/04/main.zig
+++ 2024/04/main.zig
@@ -8,7 +8,8 @@ pub fn main() !void {
     var buf: [4096]u8 = undefined;
     var map = std.ArrayList(u8).init(std.heap.page_allocator);
     defer map.deinit();
-    var res: usize = 0;
+    var res_one: usize = 0;
+    var res_two: usize = 0;
     var row_len: usize = 0;
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         row_len = @max(row_len, line.len);
@@ -17,11 +18,14 @@ pub fn main() !void {
         }
     }
     for (0..map.items.len) |idx| {
-        res += check(map.items, idx, row_len);
+        res_one += check(map.items, idx, row_len);
+        res_two += findX(map.items, idx, row_len);
     }
-    std.debug.print("Part 1: {d}\n", .{res});
+    std.debug.print("Part 1: {d}\n", .{res_one});
+    std.debug.print("Part 2: {d}\n", .{res_two});
 }
 
+// TODO: list and stuff to struct with methods/fields (WOW)
 fn check(map: []const u8, idx: usize, row_len: usize) usize {
     var res: usize = 0;
     const y: usize = @divTrunc(idx, row_len);
@@ -47,13 +51,35 @@ fn check(map: []const u8, idx: usize, row_len: usize) usize {
     }
     if (x >= 4 - 1 and y + 4 <= rows) {
         const slice = makeSlice(map, idx, row_len, -1, 1);
-        // std.debug.print("row: {d} col: {d} slice: {s}\n", .{ y, x, slice });
         if (okSlice(&slice)) {
-            // std.debug.print("yay!\n", .{});
             res += 1;
         }
     }
     return res;
+}
+
+fn findX(map: []const u8, idx: usize, row_len: usize) usize {
+    const y: usize = @divTrunc(idx, row_len);
+    const x: usize = @rem(idx, row_len);
+    const rows: usize = @divTrunc(map.len, row_len);
+    if (y >= rows - 2 or x >= row_len - 2 or map[idx + row_len + 1] != 'A') {
+        return 0;
+    }
+    const tl = map[idx];
+    const tr = map[idx + 2];
+    const bl = map[idx + 2 * row_len];
+    const br = map[idx + 2 * row_len + 2];
+    // zig fmt: off
+    if (
+           (tl == 'M' and tr == 'M' and bl == 'S' and br == 'S')
+        or (tl == 'S' and tr == 'S' and bl == 'M' and br == 'M')
+        or (tl == 'M' and tr == 'S' and bl == 'M' and br == 'S')
+        or (tl == 'S' and tr == 'M' and bl == 'S' and br == 'M')
+        // zig fmt: on
+    ) {
+        return 1;
+    }
+    return 0;
 }
 
 fn makeSlice(map: []const u8, idx: usize, row_len: usize, dx: isize, dy: usize) [4]u8 {
