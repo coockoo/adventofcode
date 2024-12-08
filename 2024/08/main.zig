9d54bb0fd6b935de90ce41db8c40c6819ba36929
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-12-08 22:22:54 +0200

    2024-08: make sure that both parts are solved

--- 2024/08/main.zig
+++ 2024/08/main.zig
@@ -1,29 +1,22 @@
 const std = @import("std");
+const Map = std.AutoHashMap(u8, std.ArrayList(usize));
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa_allocator = gpa.allocator();
-    var arena = std.heap.ArenaAllocator.init(gpa_allocator);
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
     const allocator = arena.allocator();
 
     const file = try std.fs.cwd().openFile("./2024/08/input.txt", .{});
     defer file.close();
-    var bufReader = std.io.bufferedReader(file.reader());
-    var reader = bufReader.reader();
-    var buf: [4096]u8 = undefined;
-    var res_one: usize = 0;
-    var res_two: usize = 0;
 
+    var reader = file.reader();
     var row_len: usize = 0;
     var rows: usize = 0;
-
-    var map = std.AutoHashMap(u8, std.ArrayList(usize)).init(allocator);
+    var map = Map.init(allocator);
     defer map.deinit();
-    var signals = std.AutoHashMap(usize, bool).init(allocator);
-    defer signals.deinit();
-
-    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 64)) |line| {
         row_len = @max(row_len, line.len);
         for (line, 0..) |c, idx| {
             if (c == '.') {
@@ -36,6 +29,17 @@ pub fn main() !void {
         rows += 1;
     }
 
+    const res_one = try solve(allocator, map, row_len, rows, false);
+    const res_two = try solve(allocator, map, row_len, rows, true);
+
+    std.debug.print("Part 1: {d}\n", .{res_one});
+    std.debug.print("Part 2: {d}\n", .{res_two});
+}
+
+fn solve(allocator: Allocator, map: Map, row_len: usize, rows: usize, extended: bool) !usize {
+    var signals = std.AutoHashMap(usize, bool).init(allocator);
+    defer signals.deinit();
+
     var it = map.iterator();
     while (it.next()) |entry| {
         for (entry.value_ptr.items) |a| {
@@ -43,9 +47,10 @@ pub fn main() !void {
                 if (a == b) {
                     continue;
                 }
-                // for part 1 - comment
-                try signals.put(a, true);
-                try signals.put(b, true);
+                if (extended) {
+                    try signals.put(a, true);
+                    try signals.put(b, true);
+                }
 
                 const ax = toi(@rem(a, row_len));
                 const ay = toi(@divTrunc(a, row_len));
@@ -64,17 +69,14 @@ pub fn main() !void {
                     try signals.put(@abs(ni), true);
                     nax += dx;
                     nay += dy;
-                    // for part 1
-                    // break;
+                    if (!extended) {
+                        break;
+                    }
                 }
             }
         }
     }
-
-    res_one += 0;
-    res_two += 0;
-    std.debug.print("Part 1: {d}\n", .{signals.count()});
-    std.debug.print("Part 2: {d}\n", .{res_two});
+    return signals.count();
 }
 
 fn toi(n: usize) isize {
